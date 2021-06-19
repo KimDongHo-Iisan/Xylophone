64b341d3249aaa7b083c4e56a1b27da423836259
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-19 22:41:28 +0900

    버튼을 눌렀을때, 투명해졌다가 다시 선명해지는 버튼으로 만드는 코드

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -12,15 +12,24 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
        
         print(sender.currentTitle)
-        playSound(soundName: sender.currentTitle!) //!을 붙이는 이유, currentTitle이 존재하지않는 값일수도 있기 때문에, 컴퓨터는 확신을 가지지 못한다. 그래서 값이 확실히 있는 경우에는 !을 붙여서 우리가 컴퓨터에게 확신을 주는것이다.
+        //playSound함수를 호출할때 누른 버튼의 제목에 따른 오디오를 키도록 하는 함수 currnetTitle로 사용할경우 오류가 발생하는데 이유는 선택적 문자열이라고 부르는 데이터 유형이기때문인데, 제목이 실제로 없는 버튼이 있을수 있기때문에 이러한 데이터유형을 추측하는것이다.실제로 없는 버튼이 있을 경우에는 앱이 다운되기 때문에 !를 사용해서 값이 무조건 있을것이라고 강조하는것.
+        playSound(soundName: sender.currentTitle!)
+
+        //버튼을 눌렀을때 투명도를 0.5 낮춘다.
+        sender.alpha = 0.5
+
+        //Code should execute after 0.2 second delay. 이 코드는 0.2초의 지연 시간 이후에 실행되도록 하는 코드이다.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque. 지연 시간이 끝난 이후에는 투명도를 1.0으로 다시 돌리는 코드를 사용한다.
+            sender.alpha = 1.0
+        }
+
     }
-    
+
     func playSound(soundName: String) {
-        //url을 C.wav 파일의 위치로 설정
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!) //파일을 player에 넣음
-        player.play() //소리를 재생해라.
-        //전화기가 무음 모드가 켜져있을때 소리가 나오도록 하는 부분을 제거했기 때문에 무음모드일때는 소리가 나지않는다. 
-                
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+
     }
 }
