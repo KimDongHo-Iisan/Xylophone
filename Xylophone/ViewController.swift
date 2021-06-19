3e76ed623ffe2a367b4922199e9aa8fa21975442
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-19 21:16:31 +0900

    2

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,41 @@
 //
 
 import UIKit
+import AVFoundation //오디오 하드웨어와 상호작용할수 있는 라이브러리.
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    @IBAction func KeyPressed(_ sender: Any) {
+    
+    @IBAction func KeyPressed(_ sender: UIButton) {
         print("I got pressed")
     }
     
-    
-
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
 }
 
